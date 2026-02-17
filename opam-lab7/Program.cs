eea50c0349d084c45b966a9c3675acc8c9c5caa0
lohoida-uzhnu <c.lohoida.stanislav@student.uzhnu.edu.ua>
2026-02-17 20:25:23 +0200

    task 1(no ready)

---
+++ opam-lab7/Program.cs
@@ -0,0 +1,13 @@
+﻿List<Action> actions = new List<Action>();
+
+for (int i = 1; i <= 5; i++)
+{
+    actions.Add(() => Console.WriteLine(i));
+}
+
+foreach (var action in actions)
+{
+    action();
+}
+
+
